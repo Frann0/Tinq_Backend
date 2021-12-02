3ec8108f91330236447ab8bb36ba3d46ea679cf0
dp-desktop <dnisp@live.dk>
2021-12-02 20:43:48 +0100

    Added some tests

--- qwertygroup.Domain.Test/Services/PostServiceTest.cs
+++ qwertygroup.Domain.Test/Services/PostServiceTest.cs
@@ -14,34 +14,63 @@ namespace qwertygroup.Domain.Test.Services
         private readonly IPostService _postService;
         private readonly Mock<IPostRepository> _mock;
         private readonly IPostRepository _mockPostRepository;
-        private List<Post> _expected = new List<Post>{new Post{Id = 0, TitleId=0,UserId=0,BodyId=0},
-                                                      new Post{Id = 1, TitleId=1,UserId=1,BodyId=1}};
+        private readonly Post _expectedPost = new Post { TitleId = 1231231, BodyId = 1337 };
+        private List<Post> _expected = new List<Post>{new Post{Id = 1, TitleId=0,UserId=0,BodyId=0},
+                                                      new Post{Id = 2, TitleId=1,UserId=1,BodyId=1}};
 
-        public PostServiceTest(){
+        public PostServiceTest()
+        {
             _mock = new Mock<IPostRepository>();
-            _mockPostRepository=_mock.Object;
+            _mock.Setup(r => r.GetAllPosts()).Returns(_expected);
+            _mock.Setup(r => r.CreatePost(_expectedPost)).Returns(_expectedPost);
+            _mock.Setup(r => r.DeletePost(_expected[0])).Callback(() => _expected.Remove(_expected[0]));
+            _mockPostRepository = _mock.Object;
             _postService = new PostService(_mockPostRepository);
         }
 
         [Fact]
-        public void PostService_Exists_And_Extends_IPostService(){
+        public void PostService_Exists_And_Extends_IPostService()
+        {
             IPostService ps = new PostService(_mockPostRepository);
             Assert.NotNull(ps);
             Assert.IsAssignableFrom<IPostService>(ps);
         }
 
         [Fact]
-        public void PostService_With_Null_Parameter_Throws_Exception(){
+        public void PostService_With_Null_Parameter_Throws_Exception()
+        {
             IPostService ps;
             Assert.Equal(
-            Assert.Throws<MissingFieldException>(()=> ps = new PostService(null)).Message,
+            Assert.Throws<MissingFieldException>(() => ps = new PostService(null)).Message,
             "PostService must hate a PostRepository!");
         }
 
         [Fact]
-        public void PostService_Has_GetAllPostsMethod_And_Returns_List_Of_Posts(){
-            _mock.Setup(r=>r.GetAllPosts()).Returns(_expected);
-            Assert.Equal(_postService.GetAllPosts(),_expected);
+        public void PostService_Has_GetAllPostsMethod_And_Returns_List_Of_Posts()
+        {
+            _mock.Setup(r => r.GetAllPosts()).Returns(_expected);
+            Assert.Equal(_postService.GetAllPosts(), _expected);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void PostService_Has_GetPostMethod_And_Returns_TheCorrectPost(int id)
+        {
+            Assert.Equal(_postService.GetPost(id), _expected[id - 1]);
+        }
+        [Fact]
+        public void PostService_HasCreatePostMethod_And_Returns_TheCreatedPost()
+        {
+            Assert.Equal(_postService.CreatePost(_expectedPost), _expectedPost);
+        }
+
+        [Fact]
+        public void PostService_HasDeletePostMethod()
+        {
+            int count = _expected.Count;
+            _postService.DeletePost(_expected[0]);
+            Assert.True(count > _expected.Count);
         }
     }
 }
